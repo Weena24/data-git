ca16836cd2f8a61978f3451644b2531dd26c85e5
Weena Ramen <weena.ramen@gmail.com>
2024-01-24 10:14:44 +0400

    Fix style issues, should get a 'Good Style' now 🙏

--- today.py
+++ today.py
@@ -1,3 +1,4 @@
+"""A module computing buddy pair names for the day"""
 '''
 This is the module docstring
 '''
@@ -5,12 +6,12 @@ This is the module docstring
 
 def my_name_is():
     '''
-
+"""A module computing buddy pair names for the day"""
     '''
     return "wramen"
 
 def my_buddy_is():
     '''
-
+"""A module computing buddy pair names for the day"""
     '''
     return "buddy"
